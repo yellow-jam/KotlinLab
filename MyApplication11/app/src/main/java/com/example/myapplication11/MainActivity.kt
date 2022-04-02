e78dd56cf9eb00feeab435ba9dca0767cba1b620
yellowjam <lerle144@gmail.com>
2022-04-02 17:28:01 +0900

    11-5 뷰 페이저2

--- MyApplication11/app/src/main/java/com/example/myapplication11/MainActivity.kt
+++ MyApplication11/app/src/main/java/com/example/myapplication11/MainActivity.kt
@@ -6,11 +6,32 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.SearchView // 임포트 주의 androidx
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.myapplication11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    class MyFragmentAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity){
+        val fragments: List<Fragment>
+        init {
+            fragments = listOf(Fragment1(), Fragment2(), Fragment3())
+        }
+        override fun getItemCount(): Int {
+            //TODO("Not yet implemented")
+            return fragments.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            //TODO("Not yet implemented")
+            return fragments[position]
+        }
+    }
+
+
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +41,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
-        // 프래그먼트
+        // 11 프래그먼트 매니저
+        /*
         val fragmentManager : FragmentManager = supportFragmentManager
         val transaction : FragmentTransaction = fragmentManager.beginTransaction()
         var fragment = Fragment1()
         transaction.add(R.id.fragment_content, fragment)
         transaction.commit()
+        */
+
+        // 11-5 뷰 페이저2
+        binding.viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 가로 방향으로 프래그먼트 전환
+        binding.viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 가로 방향으로 프래그먼트 전환
+        binding.viewpager.adapter = MyFragmentAdapter(this)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
