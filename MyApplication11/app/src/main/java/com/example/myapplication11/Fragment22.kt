ffc34fcf571f2d15a93caf11b82100882c3284b5
yellowjam <lerle144@gmail.com>
2022-04-13 01:04:54 +0900

    12-2 탭 구성-프래그먼트2 안에 프래그먼트21, 22, 23

--- MyApplication11/app/src/main/java/com/example/myapplication11/Fragment22.kt
+++ MyApplication11/app/src/main/java/com/example/myapplication11/Fragment22.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.myapplication11.databinding.Fragment2Binding
-import com.google.android.material.tabs.TabLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Fragment2.newInstance] factory method to
+ * Use the [Fragment22.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fragment2 : Fragment() {
+class Fragment22 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,30 +34,7 @@ class Fragment2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = Fragment2Binding.inflate(inflater, container, false)
-        binding.tabs.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                //TODO("Not yet implemented")
-                val transaction = fragmentManager?.beginTransaction()
-                    // 액티비티에서는 supportFragmentManager 사용
-                    // 프래그먼트메니저에서는 자신의 것(fragmentManager)을 직접 가져옴
-                when(tab?.text) {
-                    "tab1" -> transaction?.replace(R.id.tabContent, Fragment21())
-                    "tab2" -> transaction?.replace(R.id.tabContent, Fragment22())
-                    "tab3" -> transaction?.replace(R.id.tabContent, Fragment23())
-                }
-                transaction?.commit() // 필요
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                //TODO("Not yet implemented")
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                //TODO("Not yet implemented")
-            }
-        })
-        return binding.root
+        return inflater.inflate(R.layout.fragment_22, container, false)
     }
 
     companion object {
@@ -69,12 +44,12 @@ class Fragment2 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment2.
+         * @return A new instance of fragment Fragment22.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Fragment2().apply {
+            Fragment22().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
