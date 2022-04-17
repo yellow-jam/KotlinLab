aaf577ed6a768b14b83bfc57b4d20b098b43fc5c
unknown <lerle144@gmail.com>
2022-04-17 18:21:36 +0900

    #2 (신식) 사후처리 메소드 ActivityResultLauncher 사용하기

--- MyApplication13re/app/src/main/java/com/example/myapplication13re/MainActivity.kt
+++ MyApplication13re/app/src/main/java/com/example/myapplication13re/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication13re.databinding.ActivityMainBinding
 
@@ -19,20 +21,28 @@ class MainActivity : AppCompatActivity() {
         binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.mainRecyclerView.adapter = MyAdapter(datas)
 
+        val requestLauncher:ActivityResultLauncher<Intent>
+            = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val d3 = it.data!!.getStringExtra("test")
+            Log.d("MobileApp", d3!! )
+        }
+
         binding.fab.setOnClickListener {
             val intent = Intent(this, AddActivity::class.java)
             intent.putExtra("data1", "mobile")
             intent.putExtra("data2", "app")
-            startActivityForResult(intent, 10)//startActivity(intent)
+            //startActivityForResult(intent, 10)//startActivity(intent)
+            requestLauncher.launch(intent)
         }
     }
 
+    /*
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode==10 && resultCode== RESULT_OK) {
             val d3 = data?.getStringExtra("test")
             Log.d("MobileApp", d3!! )
         }
-
     }
+    */
 }
