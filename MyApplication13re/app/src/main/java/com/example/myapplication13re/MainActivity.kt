557a87525acf380da33be654bddb926250052bd4
unknown <lerle144@gmail.com>
2022-04-17 17:31:15 +0900

    #2 13 re 리사이클러뷰 설정

--- MyApplication13re/app/src/main/java/com/example/myapplication13re/MainActivity.kt
+++ MyApplication13re/app/src/main/java/com/example/myapplication13re/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication13re
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication13re.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datas:MutableList<String>? = null
+        binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.mainRecyclerView.adapter = MyAdapter(datas)
     }
 }
