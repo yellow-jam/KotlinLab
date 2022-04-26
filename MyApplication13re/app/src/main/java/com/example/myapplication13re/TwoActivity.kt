1732b523f3d4d21096afdec4b3cf71f5fa61e018
yellowjam <lerle144@gmail.com>
2022-04-27 02:03:07 +0900

    13-5 코루틴

--- MyApplication13re/app/src/main/java/com/example/myapplication13re/TwoActivity.kt
+++ MyApplication13re/app/src/main/java/com/example/myapplication13re/TwoActivity.kt
@@ -2,8 +2,13 @@ package com.example.myapplication13re
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import com.example.myapplication13re.databinding.ActivityTwoBinding
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consume
+import kotlinx.coroutines.channels.consumeEach
 import kotlin.system.measureTimeMillis
 
 class TwoActivity : AppCompatActivity() {
@@ -25,6 +30,25 @@ class TwoActivity : AppCompatActivity() {
             binding.tv3.text = "합계: $sum"
              */
 
+            // 스레드 안에서 만든 sum을 다른 스레드로 전달하기 위해 채널 생성
+            val channel = Channel<Long>()
+            val bgScope = CoroutineScope(Dispatchers.Default + Job())  // 백그라운드에서 작업하도록 함
+            bgScope.launch {
+                var sum = 0L
+                var time = measureTimeMillis {
+                    for(i in 1..4000000000) {
+                        sum += i
+                    }
+                }
+                Log.d("mobileApp", "걸린 시간 : $time")
+                channel.send(sum)
+            }
+            val mainScope = GlobalScope.launch(Dispatchers.Main) {
+                channel.consumeEach {
+                    binding.tv3.text = "합계: $it"  // sum이 it으로 표현됨
+                }
+
+            }
         }
     }
 }
