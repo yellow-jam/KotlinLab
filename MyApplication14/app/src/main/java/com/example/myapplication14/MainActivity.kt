0907516a2c3b81b87a9f4d98d8909f155079376e
yellowjam <lerle144@gmail.com>
2022-04-27 03:02:05 +0900

    #4 리시버를 사용하지 않고(메인액티비티 코드만으로) 시스템 정보를 얻어오는 방법

--- MyApplication14/app/src/main/java/com/example/myapplication14/MainActivity.kt
+++ MyApplication14/app/src/main/java/com/example/myapplication14/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.myapplication14
 
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myapplication14.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        /*
         val receiver = MyReceiver()
         val filter = IntentFilter("ACTION_RECEIVER").apply {
             addAction(Intent.ACTION_BATTERY_LOW)
@@ -22,7 +25,21 @@ class MainActivity : AppCompatActivity() {
             addAction(Intent.ACTION_POWER_DISCONNECTED)
         }
         registerReceiver(receiver, filter)  // (리시버, 필터)
+         */
 
+        registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))!!.apply {
+            val status = getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+            if(status == BatteryManager.BATTERY_STATUS_CHARGING){
+                val chargePlug = getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
+                when(chargePlug){
+                    BatteryManager.BATTERY_PLUGGED_AC -> Log.d("mobileApp", "AC CHARGED")
+                    BatteryManager.BATTERY_PLUGGED_USB -> Log.d("mobileApp", "USB CHARGED")
+                }
+            }
+            else {
+                Log.d("mobileApp", "DISCHARGING")
+            }
+        }
         binding.button.setOnClickListener {
             val intent = Intent(this, MyReceiver::class.java)
             sendBroadcast(intent)
