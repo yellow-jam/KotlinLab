add4e88d12be507b1eabb1994ce3d318f9ffafce
yellowjam <lerle144@gmail.com>
2022-04-27 02:14:43 +0900

    #4 14장 리시버 생성(매니페스트에 자동 등록), MainActivity에서 코틀린 코드로도 등록

--- MyApplication14/app/src/main/java/com/example/myapplication14/MainActivity.kt
+++ MyApplication14/app/src/main/java/com/example/myapplication14/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication14
 
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication14.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val receiver = MyReceiver()
+        val filter = IntentFilter("ACTION_RECEIVER")
+        registerReceiver(receiver, filter)  // (리시버, 필터)
     }
 }
