416ff3e1b5505aa1aaf0843286a631106eb38cec
yellowjam <lerle144@gmail.com>
2022-04-27 16:27:35 +0900

    #5 주소록과 연동하기

--- MyApplication16/app/src/main/java/com/example/myapplication16/MainActivity.kt
+++ MyApplication16/app/src/main/java/com/example/myapplication16/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myapplication16
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.myapplication16.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 사후 처리 메소드
+        val requestContractLauncher: ActivityResultLauncher<Intent> =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                if(it.resultCode == RESULT_OK){
+                    Log.d("mobileApp", "${it.data?.data}")
+                }
+            }
+        binding.addrBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            requestContractLauncher.launch(intent)
+        }
     }
 }
