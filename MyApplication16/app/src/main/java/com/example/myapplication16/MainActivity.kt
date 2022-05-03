f5ee71c331cb63a254bde10f1d23cc0786bf8f6f
yellowjam <lerle144@gmail.com>
2022-05-04 01:41:13 +0900

    #5 전화 앱 연동하기

--- MyApplication16/app/src/main/java/com/example/myapplication16/MainActivity.kt
+++ MyApplication16/app/src/main/java/com/example/myapplication16/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication16
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
@@ -12,6 +14,7 @@ import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.example.myapplication16.databinding.ActivityMainBinding
 import java.io.File
@@ -129,7 +132,23 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo: 37.562952, 126.9779451"))  // 지도 프로토콜 geo - 위치정보 위도, 경도
             startActivity(intent)
         }
-        
+
+        /* 전화 앱 연동 */
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission() ) {  // 여기 들어가는 함수 주의
+
+        }
+
+        binding.callBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel: 010-9072-3635"))
+            val status = ContextCompat.checkSelfPermission(this, "android.permission.CALL_PHONE")  // 현재의 권한 허용 상태 확인
+            if(status == PackageManager.PERMISSION_GRANTED){
+                startActivity(intent)
+            }
+            else {
+                requestPermissionLauncher.launch("android.permission.CALL_PHONE")
+            }
+        }
 
     }
 
