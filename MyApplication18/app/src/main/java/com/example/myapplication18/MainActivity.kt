f13086e04d5d7dfe773306c0d3983dad30a2b34f
yellowjam <lerle144@gmail.com>
2022-05-10 05:51:41 +0900

    #7 getSystemService()로 네트워크 연결 정보 받아오기

--- MyApplication18/app/src/main/java/com/example/myapplication18/MainActivity.kt
+++ MyApplication18/app/src/main/java/com/example/myapplication18/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication18
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,12 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         // permission 사용자 허가
         checkPermissions()
-
-
         // 폰 정보
         binding.tv.text = getPhoneInfo()
-
         // 연결 정보
+        binding.tv.text = binding.tv.text.toString() + getConnectivity()
 
 
     }
@@ -48,6 +48,26 @@ class MainActivity : AppCompatActivity() {
             val phoneNum = manager.line1Number  // 스마트폰의 전화번호
 
             return "contryIso: $countryIso\noperatorName: $operatorName\nphoneNum: $phoneNum"
+        }
+        return ""
+    }
+
+    // 왜 연결되지 않았는가를 텍스트뷰에 출력 : String 타입으로
+    private fun getConnectivity() : String {
+        val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){  // 24 이상
+            val nw = manager.activeNetwork
+            if(nw == null) return "activeNetwork NULL"
+            val actNw = manager.getNetworkCapabilities(nw)  // 네트워크 용량 확인
+            if(actNw == null) return "activeNetwork - Capabilities NULL"  // 네트워크는 있는데 용량이 없는 경우
+
+            // 네트워크 상태(유형) 확인
+            if(actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) return "cellular available"
+            else if(actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) return "wifi available"
+            else return "available"
+        } else {  // 24 이하에서는 연결되었는지 안되었는지만 판단
+            if(manager.activeNetworkInfo!!.isConnected) return "activeNetworkInfo is Connected"
+            return "activeNetworkInfo is NOT Connected"
         }
         return ""
     }
