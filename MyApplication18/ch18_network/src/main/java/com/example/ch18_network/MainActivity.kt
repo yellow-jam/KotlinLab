225c284942300f0f71dba4a5fd2cd1e6496427c9
yellowjam <lerle144@gmail.com>
2022-05-10 06:26:58 +0900

    #7 매니페스트 설정, gradle에 라이브러리 implement

--- MyApplication18/ch18_network/src/main/java/com/example/ch18_network/MainActivity.kt
+++ MyApplication18/ch18_network/src/main/java/com/example/ch18_network/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.ch18_network
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ch18_network.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
