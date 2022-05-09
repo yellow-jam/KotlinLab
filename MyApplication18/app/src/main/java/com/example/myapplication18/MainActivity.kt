808ba78b071f48d99f9b2e4b3fd41bbe03cda435
yellowjam <lerle144@gmail.com>
2022-05-10 05:39:55 +0900

    #7 getSystemService()로 폰 정보 받아오기

--- MyApplication18/app/src/main/java/com/example/myapplication18/MainActivity.kt
+++ MyApplication18/app/src/main/java/com/example/myapplication18/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication18
 
 import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.TelephonyManager
+import androidx.core.app.ActivityCompat
 import com.example.myapplication18.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
 
         // 폰 정보
-
+        binding.tv.text = getPhoneInfo()
 
         // 연결 정보
 
@@ -32,5 +36,19 @@ class MainActivity : AppCompatActivity() {
         else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){  // 29 이하 24 이상
             requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), REQUEST_CODE)
         }
+    }
+
+    private fun getPhoneInfo() : String {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) == PackageManager.PERMISSION_GRANTED
+          || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED){
+
+            val manager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            val countryIso = manager.networkCountryIso  // 네트워크 제공 국가
+            val operatorName = manager.networkOperatorName  // 네트워크 제공 사업자
+            val phoneNum = manager.line1Number  // 스마트폰의 전화번호
+
+            return "contryIso: $countryIso\noperatorName: $operatorName\nphoneNum: $phoneNum"
+        }
+        return ""
     }
 }
