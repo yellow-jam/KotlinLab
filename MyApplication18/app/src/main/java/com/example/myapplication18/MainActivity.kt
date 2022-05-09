495b97ac44aa34febe97caf4ba4621143a299d8a
yellowjam <lerle144@gmail.com>
2022-05-10 05:29:56 +0900

    #7 퍼미션 설정, 사용자에게 허가받기

--- MyApplication18/app/src/main/java/com/example/myapplication18/MainActivity.kt
+++ MyApplication18/app/src/main/java/com/example/myapplication18/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication18
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication18.databinding.ActivityMainBinding
@@ -9,5 +11,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // permission 사용자 허가
+        checkPermissions()
+
+
+        // 폰 정보
+
+
+        // 연결 정보
+
+
+    }
+
+    private fun checkPermissions(){
+        val REQUEST_CODE = 1001  // int 타입
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){ // 30 이상
+            requestPermissions(arrayOf(Manifest.permission.READ_PHONE_NUMBERS), REQUEST_CODE)
+        }
+        else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){  // 29 이하 24 이상
+            requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), REQUEST_CODE)
+        }
     }
 }
