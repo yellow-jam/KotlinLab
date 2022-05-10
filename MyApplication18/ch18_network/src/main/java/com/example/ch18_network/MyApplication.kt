7bca4e862932c5a5bda448d1767b3291f50a06da
yellowjam <lerle144@gmail.com>
2022-05-11 00:31:34 +0900

    #7 MyApplication.kt에 컨버터 추가

--- MyApplication18/ch18_network/src/main/java/com/example/ch18_network/MyApplication.kt
+++ MyApplication18/ch18_network/src/main/java/com/example/ch18_network/MyApplication.kt
@@ -1,19 +1,29 @@
 package com.example.ch18_network
 
 import android.app.Application
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MyApplication:Application() {
     companion object{
         var networkService : NetworkService
+        var networkServiceXml : NetworkService
         val retrofit : Retrofit
         get() = Retrofit.Builder()
             .baseUrl("https://api.odcloud.kr/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+        val parser = TikXml.Builder().exceptionOnUnreadXml(false).build()
+        val retrofitXml : Retrofit
+            get() = Retrofit.Builder()
+                .baseUrl("http://apis.data.go.kr/")
+                .addConverterFactory(TikXmlConverterFactory.create(parser))
+                .build()
         init{
             networkService = retrofit.create(NetworkService::class.java)
+            networkServiceXml = retrofitXml.create(NetworkService::class.java)
         }
     }
 }
