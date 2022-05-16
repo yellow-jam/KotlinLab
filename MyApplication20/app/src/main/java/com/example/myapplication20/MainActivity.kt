51837821f4cf2427e82d78be5e9af33d8b728e2f
yellowjam <lerle144@gmail.com>
2022-05-17 04:13:54 +0900

    #8 Auth 액티비티, 관련 파일 생성

--- MyApplication20/app/src/main/java/com/example/myapplication20/MainActivity.kt
+++ MyApplication20/app/src/main/java/com/example/myapplication20/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            // 로그인: 별도의 액티비티를 만들어 처리
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
