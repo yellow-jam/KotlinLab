bd5cd47732394ba61d5e0bbe61cee87c29f3a4f5
yellowjam <lerle144@gmail.com>
2022-05-17 02:55:04 +0900

    #7 MyAdapter에서 Glide를 사용해서 url로 이미지를 가져오기

---
+++ MyApplication18/ch18_image/src/main/java/com/example/ch18_image/MyAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.ch18_image
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.ch18_image.databinding.ItemMainBinding
+
+class MyViewHolder(val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root)
+class MyAdapter(val context: Context, val datas: MutableList<ItemModel>?): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun getItemCount(): Int {
+        return datas?.size ?: 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+        val model = datas!![position]
+        binding.country.text = model.country_nm
+        Glide.with(binding.root)
+            .load(model.download_url)
+            .into(binding.flag)
+    }
+}
