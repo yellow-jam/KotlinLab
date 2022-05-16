105bd8571275f1b7ff07ef977b4f8be1201ae7e0
yellowjam <lerle144@gmail.com>
2022-05-17 05:32:46 +0900

    #8 MainActivity: AuthActivity에서 돌아온 후 / 회원가입 오류 해결

--- MyApplication20/app/src/main/java/com/example/myapplication20/MainActivity.kt
+++ MyApplication20/app/src/main/java/com/example/myapplication20/MainActivity.kt
@@ -6,10 +6,11 @@ import android.os.Bundle
 import com.example.myapplication20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
@@ -20,6 +21,23 @@ class MainActivity : AppCompatActivity() {
             else if(binding.btnLogin.text.equals("로그아웃")) // 로그인 상태
                 intent.putExtra("data", "login")
             startActivity(intent)
+        }
+    }
+
+    // AuthActivity에서 돌아온 후
+    override fun onStart() {
+        super.onStart()
+        if(MyApplication.checkAuth()){  // 검증된 이메일인가
+            // 로그인 상태
+            binding.btnLogin.text = "로그아웃"
+            binding.authTv.text = "${MyApplication.email}님 반갑습니다."
+            binding.authTv.textSize = 16F
+        }
+        else{
+            // 로그아웃 상태
+            binding.btnLogin.text = "로그인"
+            binding.authTv.text = "덕성 모바일"
+            binding.authTv.textSize = 24F
         }
     }
 }
