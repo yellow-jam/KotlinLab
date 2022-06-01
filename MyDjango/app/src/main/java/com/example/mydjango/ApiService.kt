5fcbaa5dedef3621e778dbd3cde6045e93c4d495
yellowjam <lerle144@gmail.com>
2022-06-01 15:02:56 +0900

    #10 장고서버에서 데이터 불러오기, 서버에 데이터 추가하기 xml

--- MyDjango/app/src/main/java/com/example/mydjango/ApiService.kt
+++ MyDjango/app/src/main/java/com/example/mydjango/ApiService.kt
@@ -1,5 +1,12 @@
 package com.example.mydjango
 
-interface ApiService {
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface ApiService {
+    @GET("test")
+    fun getList(
+        @Query("format") returnType: String
+    ): Call<MutableList<hInfoModel>>
 }
--- MyDjango/app/src/main/java/com/example/mydjango/MainActivity.kt
+++ MyDjango/app/src/main/java/com/example/mydjango/MainActivity.kt
@@ -2,10 +2,62 @@ package com.example.mydjango
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mydjango.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDjango.setOnClickListener {
+            var call: Call<MutableList<hInfoModel>> = MyApplication.apiService.getList("json")
+            call?.enqueue(object: Callback<MutableList<hInfoModel>> {
+                override fun onResponse(
+                    call: Call<MutableList<hInfoModel>>,
+                    response: Response<MutableList<hInfoModel>>
+                ) {
+                    if(response.isSuccessful){
+                        binding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                        binding.recyclerView.adapter = hInfoAdapter(this@MainActivity, response.body()?.toMutableList<hInfoModel>())
+                    }
+                }
+
+                override fun onFailure(call: Call<MutableList<hInfoModel>>, t: Throwable) {
+                    Log.d("mobileApp", "error.......")
+                    Log.d("mobileApp", call.request().toString())
+                }
+
+            })
+        }
+
+        binding.btnPost.setOnClickListener {
+            binding.layout1.visibility = View.VISIBLE
+            binding.layout2.visibility = View.VISIBLE
+            binding.layout3.visibility = View.VISIBLE
+            binding.btnPost2.visibility = View.VISIBLE
+        }
+        binding.btnPost2.setOnClickListener {
+            val name = binding.edtName.text.toString()
+            val phone = binding.edtPhone.text.toString()
+            val addr = binding.edtAddr.text.toString()
+            if(name!="" && phone!="" && addr!=""){
+
+            }
+            else{
+                Toast.makeText(this, "데이터를 모두 입력하세요", Toast.LENGTH_SHORT).show()
+            }
+            binding.layout1.visibility = View.GONE
+            binding.layout2.visibility = View.GONE
+            binding.layout3.visibility = View.GONE
+            binding.btnPost2.visibility = View.GONE
+        }
     }
 }
