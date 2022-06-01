ea372044ba4161f060b2b901f23d4697abd82954
yellowjam <lerle144@gmail.com>
2022-06-01 15:23:33 +0900

    #10 사용자의 입력을 받아 Django 서버에 저장하고 변경된 데이터를 표시

--- MyDjango/app/src/main/java/com/example/mydjango/ApiService.kt
+++ MyDjango/app/src/main/java/com/example/mydjango/ApiService.kt
@@ -1,7 +1,9 @@
 package com.example.mydjango
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiService {
@@ -9,4 +11,9 @@ interface ApiService {
     fun getList(
         @Query("format") returnType: String
     ): Call<MutableList<hInfoModel>>
+
+    @POST("test/")  // test/ 뒤에 슬래시 필수
+    fun postData(
+        @Body data:hInfoModel
+    ): Call<hInfoModel>
 }
--- MyDjango/app/src/main/java/com/example/mydjango/MainActivity.kt
+++ MyDjango/app/src/main/java/com/example/mydjango/MainActivity.kt
@@ -12,30 +12,14 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnDjango.setOnClickListener {
-            var call: Call<MutableList<hInfoModel>> = MyApplication.apiService.getList("json")
-            call?.enqueue(object: Callback<MutableList<hInfoModel>> {
-                override fun onResponse(
-                    call: Call<MutableList<hInfoModel>>,
-                    response: Response<MutableList<hInfoModel>>
-                ) {
-                    if(response.isSuccessful){
-                        binding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-                        binding.recyclerView.adapter = hInfoAdapter(this@MainActivity, response.body()?.toMutableList<hInfoModel>())
-                    }
-                }
-
-                override fun onFailure(call: Call<MutableList<hInfoModel>>, t: Throwable) {
-                    Log.d("mobileApp", "error.......")
-                    Log.d("mobileApp", call.request().toString())
-                }
-
-            })
+            getList()
         }
 
         binding.btnPost.setOnClickListener {
@@ -49,15 +33,52 @@ class MainActivity : AppCompatActivity() {
             val phone = binding.edtPhone.text.toString()
             val addr = binding.edtAddr.text.toString()
             if(name!="" && phone!="" && addr!=""){
+                val data = hInfoModel(name, phone, addr)
+                var call:Call<hInfoModel> = MyApplication.apiService.postData(data)
+                call?.enqueue(object:Callback<hInfoModel>{
+                    override fun onResponse(
+                        call: Call<hInfoModel>,
+                        response: Response<hInfoModel>
+                    ) {
+                        getList()
+                    }
 
+                    override fun onFailure(call: Call<hInfoModel>, t: Throwable) {
+                        Log.d("mobileApp", "error.......")
+                    }
+                })
             }
             else{
                 Toast.makeText(this, "데이터를 모두 입력하세요", Toast.LENGTH_SHORT).show()
             }
+            binding.edtName.text.clear()
+            binding.edtPhone.text.clear()
+            binding.edtAddr.text.clear()
             binding.layout1.visibility = View.GONE
             binding.layout2.visibility = View.GONE
             binding.layout3.visibility = View.GONE
             binding.btnPost2.visibility = View.GONE
         }
+    }
+
+    private fun getList() {
+        var call: Call<MutableList<hInfoModel>> = MyApplication.apiService.getList("json")
+        call?.enqueue(object: Callback<MutableList<hInfoModel>> {
+            override fun onResponse(
+                call: Call<MutableList<hInfoModel>>,
+                response: Response<MutableList<hInfoModel>>
+            ) {
+                if(response.isSuccessful){
+                    binding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                    binding.recyclerView.adapter = hInfoAdapter(this@MainActivity, response.body()?.toMutableList<hInfoModel>())
+                }
+            }
+
+            override fun onFailure(call: Call<MutableList<hInfoModel>>, t: Throwable) {
+                Log.d("mobileApp", "error.......")
+                Log.d("mobileApp", call.request().toString())
+            }
+
+        })
     }
 }
